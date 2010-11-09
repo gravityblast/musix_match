288475c8b8170a238ba1b59e82c8a7989097052e
Andrea Franz <andrea@gravityblast.com>
2010-11-09 12:36:14 -0500

    added script_tracking_url

--- lib/musix_match/models/lyrics.rb
+++ lib/musix_match/models/lyrics.rb
@@ -3,7 +3,7 @@ module MusixMatch
     class Lyrics
       include Model
       
-      model_with_attributes :lyrics_id, :lyrics_body, :track_name, :artist_name
+      model_with_attributes :lyrics_id, :lyrics_body, :track_name, :artist_name, :script_tracking_url
     end
   end
 end
