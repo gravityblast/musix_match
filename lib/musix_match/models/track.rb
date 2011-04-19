cb70329ae1277d0b20a0817565b8ade2b5c09c69
Andrea Franz <andrea@gravityblast.com>
2011-04-19 15:17:27 +0200

    added tracks attributes

--- lib/musix_match/models/track.rb
+++ lib/musix_match/models/track.rb
@@ -3,7 +3,9 @@ module MusixMatch
     class Track
       include Model
       
-      model_with_attributes :track_id, :track_mbid, :lyrics_id, :track_name, :artist_id, :artist_mbid, :artist_name
+      model_with_attributes :track_id, :track_mbid, :track_name, :track_mbid, :track_length, :lyrics_id, :instrumental, :subtitle_id,
+                            :artist_id, :artist_mbid, :artist_name, :artist_mbid,
+                            :album_name, :album_id, :album_coverart_100x100                        
       
       def self.get_chart(options={})        
         API::TrackChart.get_chart(options)
