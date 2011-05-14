9c94f6c37439e0868c43110c545dda3e2e6a2a52
Andrea Franz <andrea@gravityblast.com>
2011-05-14 07:51:36 +0200

    updated README

--- lib/musix_match/models/track.rb
+++ lib/musix_match/models/track.rb
@@ -6,7 +6,7 @@ module MusixMatch
       
       model_with_attributes :track_id, :track_mbid, :track_name, :track_mbid, :track_length, :lyrics_id, :instrumental, :subtitle_id,
                             :artist_id, :artist_mbid, :artist_name, :artist_mbid,
-                            :album_name, :album_id, :album_coverart_100x100                        
+                            :album_name, :album_id, :album_coverart_100x100
       
       def self.get_chart(options={})        
         API::TrackChart.get_chart(options)
