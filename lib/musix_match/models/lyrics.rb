5614a7835586fca98ba3063d4eefb53d80ce3904
Andrea Franz <andrea@gravityblast.com>
2011-07-07 00:52:43 +0200

    added restricted attribute to lyrics

--- lib/musix_match/models/lyrics.rb
+++ lib/musix_match/models/lyrics.rb
@@ -3,7 +3,7 @@ module MusixMatch
     class Lyrics
       include Model
       
-      model_with_attributes :lyrics_id, :lyrics_body, :lyrics_language, :script_tracking_url, :lyrics_copyright, :track_name, :artist_name
+      model_with_attributes :lyrics_id, :lyrics_body, :lyrics_language, :script_tracking_url, :lyrics_copyright, :track_name, :artist_name, :restricted
     end
   end
 end
